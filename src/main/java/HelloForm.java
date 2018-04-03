db2181baa03a70d71eb841a60cd14cf90a411db1
Suraj <Suraj.Chaunal@iiitb.org>
2018-04-03 17:43:51 +0530

    pom_changed

--- src/main/java/HelloForm.java
+++ src/main/java/HelloForm.java
@@ -46,7 +46,7 @@ public class HelloForm extends HttpServlet {
 		Class.forName("com.mysql.jdbc.Driver");  
 		  // Execute SQL query
 		Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/P1", "root", "root123");
+                "jdbc:mysql://172.17.0.3:3306/P1", "root", "hello123");
 
 		Statement stmt = con.createStatement();
 	    
